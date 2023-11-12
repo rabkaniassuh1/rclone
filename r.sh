e48acc1688c7ab828f146422056a28fe170bd6c4
rabkaniassuh1 <150588011+rabkaniassuh1@users.noreply.github.com>
2023-11-12 17:59:50 +0530

    Update r.sh

--- r.sh
+++ r.sh
@@ -2,4 +2,4 @@
 rclone config create mega mega user rabkaniassuh1@proton.me pass Colab2drive
 ln -s /bin/fusermount /bin/fusermount3
 mkdir -p /content/mega
-rclone mount mega: /content/mega --copy-links --no-gzip-encoding --no-check-certificate --allow-non-empty --allow-non-empty --umask 000 --vfs-cache-mode writes
+rclone mount mega: /content/mega 
