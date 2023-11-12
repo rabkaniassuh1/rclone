07d61c4be2af9486fed8d8bccc740e5ca25edf1f
rabkaniassuh1 <150588011+rabkaniassuh1@users.noreply.github.com>
2023-11-12 17:41:57 +0530

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Create a new remote for MEGA
-rclone config create mega mega rabkaniassuh1@proton.me Colab2drive
-
+rclone config create mega mega user rabkaniassuh1@proton.me pass Colab2drive
 ln -s /bin/fusermount /bin/fusermount3
-mkdir /content/mega
-
-# Mount the MEGA drive to a local directory
+mkdir -p /content/mega
 rclone mount mega: /content/mega --copy-links --no-gzip-encoding --no-check-certificate --allow-non-empty --allow-non-empty --umask 000 --vfs-cache-mode writes
